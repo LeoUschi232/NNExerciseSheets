fe19531fa713e2c06eae8ca4db1925a1170b0c1d
Leonard Uscinowicz <leouscin@gmail.com>
2025-07-16 17:13:40 +0200

    Update NNSheet2Exercise1.py

--- NeuralNetworksSheet2/src/NNSheet2Exercise1.py
+++ NeuralNetworksSheet2/src/NNSheet2Exercise1.py
@@ -1,68 +1,62 @@
 from matplotlib.ticker import LinearLocator
-import matplotlib.pyplot as plt
-import numpy as np
-from math import *
-from matplotlib import cm
+from matplotlib import pyplot as plt
+from numpy import pi, sin, array, meshgrid
 
 # Constants
-samerange = 2501
-length = 1
+xmax = 150
+xrange = range(xmax + 1)
 c = 20.0
-xrange = samerange
-trange = samerange
-dx = 0.0004
-dt = 0.00002
-xmax = xrange - 1
-tmax = trange - 1
-k = 4 * pi / length
+dx = 1 / xmax
+dt = 0.05 / xmax
+k = 4 * pi
 multiplier = (c * c * dt * dt / (dx * dx))
 
 # Time Steps
-xsteps = np.array([dx * i for i in range(xrange)])
-tsteps = np.array([dt * i for i in range(trange)])
+xsteps = array([dx * i for i in xrange])
+tsteps = array([dt * i for i in xrange])
 
 # Function Psi
 # calculated using list comprehensions as designed
-# For explenation look up the manual calculations in
-psi = np.array([np.array([0.0 for x in xsteps]) for t in tsteps])
-psi[0] = np.array(
-    [0 if (xn == 0 or xn == xrange - 1) else
-     5 * sin(k * xsteps[xn]) for xn in range(xrange)])
-psi[1] = np.array([0 if (xn == 0 or xn == xmax)
-                   else 5 * sin(k * xsteps[xn]) + multiplier *
-                        (2.5 * sin(k * xsteps[xn - 1]) +
-                         2.5 * sin(k * xsteps[xn + 1]) -
-                         5 * sin(k * xsteps[xn])) for xn in range(xrange)])
-for tn in range(1, tmax):
-    psi[tn + 1] = np.array([0 if (xn == 0 or xn == xmax)
-                            else 2 * psi[tn, xn] - psi[tn - 1, xn] + multiplier * (
-            psi[tn, xn - 1] + psi[tn, xn + 1] - 2 * psi[tn, xn]) for xn in range(xrange)])
+# For explenation look up the manual calculations
+psi = array([array([0.0 for x in xsteps]) for t in tsteps])
+psi[0] = array([
+    0 if (xn == 0 or xn == xmax) else
+    5 * sin(k * xsteps[xn]) for xn in xrange
+])
+psi[1] = array([
+    0 if (xn == 0 or xn == xmax) else
+    5 * sin(k * xsteps[xn]) + multiplier
+    * (2.5 * sin(k * xsteps[xn - 1]) + 2.5 * sin(k * xsteps[xn + 1]) - 5 * sin(k * xsteps[xn]))
+    for xn in xrange
+])
+for tn in range(1, xmax):
+    psi[tn + 1] = array([
+        0 if (xn == 0 or xn == xmax)
+        else 2 * psi[tn, xn] - psi[tn - 1, xn] + multiplier * (psi[tn, xn - 1] + psi[tn, xn + 1] - 2 * psi[tn, xn])
+        for xn in xrange
+    ])
 
 # Setting the parameters for plotting via matplotlib
-figconst1 = 111
-figure = plt.figure()
-axes = figure.add_subplot(figconst1, projection='3d')
-(x, y) = np.meshgrid(xsteps, tsteps)
+axes = plt.figure().add_subplot(projection="3d")
+(x, y) = meshgrid(xsteps, tsteps)
 z = psi
 
 # Create an empty array of strings with the same shape as the meshgrid, and
 # populate it with two colors in a checkerboard pattern.
-colortuple = ('yellow', 'blue')
-colors = np.array(
-    [np.array([colortuple[int((xn + yn) % len(colortuple))] for xn in range(xrange)]) for yn in range(trange)])
+colortuple = ("red", "blue")
+colors = array([
+    array([
+        colortuple[int((xn + yn) % len(colortuple))] for xn in xrange
+    ]) for yn in xrange
+])
 
-# Plotting function
-colormap_index = 2
-
-if colormap_index == 1:
-    axes.plot_surface(x, y, z, facecolors=colors)
-elif colormap_index == 2:
-    axes.plot_surface(x, y, z, cmap=cm.coolwarm)
+# Plotting function.
+axes.plot_surface(x, y, z, facecolors=colors, rcount=len(xsteps), ccount=len(tsteps))
 
 # Customize the z axis.
 axes.zaxis.set_major_locator(LinearLocator(6))
 axes.set_xlabel(r"$x\,[\mathrm{m}]$")
 axes.set_ylabel(r"$t\,[\mathrm{s}]$")
 axes.set_zlabel(r"$\psi\,(x,t)$")
-
+plt.tight_layout()
 plt.show()
